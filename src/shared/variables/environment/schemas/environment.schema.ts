4f2cb2417868b21ba9ce2d5406f51138cc2f1371
Flidix <marko.lazarchuk@gmail.com>
2024-04-27 17:21:48 +0300

    update:database connection

--- src/shared/variables/environment/schemas/environment.schema.ts
+++ src/shared/variables/environment/schemas/environment.schema.ts
@@ -7,20 +7,10 @@ export const EnvironmentSchema = z
       PORT: z.coerce.number().positive(),
       API_PREFIX: z.string().startsWith('/'),
       ALLOWED_ORIGINS: z.string(),
-      DATABASE_HOST: z.string().min(1),
-      DATABASE_PORT: z.coerce.number().positive(),
-      DATABASE_USER: z.string().min(1),
-      DATABASE_PASSWORD: z.string(),
-      DATABASE_NAME: z.string().min(1),
-      STRIPE_SECRET: z.string(),
-      NODE_MAILER_AUTH_EMAIL: z.string(),
-      SEND_FROM_EMAIL: z.string(),
-      PASSWORD: z.string(),
-      S3_BUCKET_ACCESS_KEY_ID: z.string(),
-      S3_BUCKET_SECRET_ACCESS_KEY: z.string(),
-      S3_BUCKET_USER_DEfAULT_PATH: z.string(),
+      DATABASE_URL: z.string(),
       JWT_SECRET: z.string(),
     },
+    // eslint-disable-next-line camelcase
     { required_error: '.env file is required' },
   )
   .superRefine((environment, ctx) => {

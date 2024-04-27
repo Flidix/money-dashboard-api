b7992e7524adf4611b34cb35ad971ea9ebb494a4
Lenovo <kurtakvladyslav@gmail.com>
2024-04-27 17:30:56 +0300

    feat: signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 
 import { AuthDto } from './dto/auth.dto';
-import { RefreshTokenDto } from './dto/refresh-token.dto';
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+  @Post('register')
+  signIn(@Body() body: AuthDto) {
+    return this.authService.singnUp(body);
+  }
 
+  // @Post('login')
+  // signId(@Body() body: AuthDto) {
+  //   return this.authService.signIn(body);
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,39 @@
 import { sign } from 'jsonwebtoken';
-
 import { Environment } from '@shared/variables/environment';
-
 import { DatabaseService } from '@shared/database/services/database.service';
-
-
 import { Injectable } from '@nestjs/common/decorators';
+import { AuthDto } from './dto/auth.dto';
+import { genSalt, hash } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService extends DatabaseService {
+
+  async singnUp(body: AuthDto) {
+
+    const saltRounds = 10;
+    const salt = await genSalt(saltRounds);
+
+    const hashedPassword = await hash(body.password, salt);
+
+    const user = await this.database.users.create({
+      ...body,
+      password: hashedPassword,
+    });
+
+    const payload = { id: user.id };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: '15h',
+    });
+
+    return { token: accessToken, user };
+
+  }
+
+  // async signIn(body: AuthDto) {
+
+  // }
 
   async issueAccessToken(userId: number, userEmail: string) {
     const refreshToken = await sign({ userId, email: userEmail }, Environment.JWT_SECRET, {

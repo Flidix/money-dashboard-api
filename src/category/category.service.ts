d57d99baf8d5ceb246529a150885b1c767a61150
Flidix <marko.lazarchuk@gmail.com>
2024-04-27 22:45:19 +0300

    feat: create category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+import { DatabaseService } from '@shared/database/services/database.service';
+
 @Injectable()
-export class CategoryService {}
+export class CategoryService extends DatabaseService {
+  async createCategory(title: string) {
+    const category = await this.database.categories.create({ title });
+    return category;
+  }
+}

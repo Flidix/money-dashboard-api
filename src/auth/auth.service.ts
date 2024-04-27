8dd3aba4349b4dc1bd5b196c0f348a70cc96ccbc
Flidix <marko.lazarchuk@gmail.com>
2024-04-27 16:34:19 +0300

    delete: imports

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,20 @@
-import * as bcrypt from 'bcryptjs';
-import { genSalt, hash } from 'bcryptjs';
-import { JwtPayload, sign, verify } from 'jsonwebtoken';
-import { createTransport } from 'nodemailer';
+import { sign } from 'jsonwebtoken';
 
 import { Environment } from '@shared/variables/environment';
 
 import { DatabaseService } from '@shared/database/services/database.service';
 
-import { AuthDto } from './dto/auth.dto';
-import { RefreshTokenDto } from './dto/refresh-token.dto';
-
-import { authEmailPage } from './pages/auth-email.page';
 import { Injectable } from '@nestjs/common/decorators';
-import { BadRequestException, UnauthorizedException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService extends DatabaseService {
 
   async issueAccessToken(userId: number, userEmail: string) {
-    const refreshToken = await sign({ userId: userId, email: userEmail }, Environment.JWT_SECRET, {
+    const refreshToken = await sign({ userId, email: userEmail }, Environment.JWT_SECRET, {
       expiresIn: '1h',
     });
 
-    const accessToken = await sign({ userId: userId }, Environment.JWT_SECRET, {
+    const accessToken = await sign({ userId }, Environment.JWT_SECRET, {
       expiresIn: '7d',
     });
     return { refreshToken, accessToken };
